0c3499d6f28cc316b470f46fc647b2cfc216c9a9
Isaac Kwon <you@example.com>
2017-09-25 17:51:23 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const exphbs = require("express-handlebars");
+const port = 3000;
+const app = express();
