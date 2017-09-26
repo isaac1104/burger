ba9e9c06b04c078d9d1cfe2ce0800b7c561af843
Isaac Kwon <you@example.com>
2017-09-25 18:58:37 -0700

    Basic Setup

--- server.js
+++ server.js
@@ -2,5 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 const port = 3000;
 const app = express();
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  port: 8889,
+  database: "burgers_db"
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req,res) => {
+  connection.query("SELECT * FROM burgers", (err,data) => {
+    if (err) throw err;
+    res.render("index", {burger: data});
+  });
+});
+
+app.post("/", (req,res) => {
+  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burgers], (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.listen(port, () => {
+  console.log("server listening on " + port);
+});
