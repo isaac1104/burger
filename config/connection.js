5587516ed43a535e93136f49dea4cc0dadaf5d4d
Isaac Kwon <you@example.com>
2017-09-27 21:30:55 -0700

    Initial Commit

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  port: 8889,
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    port: 8889,
+    database: "burgers_db"
+  });
+}
 
 connection.connect((err) => {
   if (err) {
